96864565ba32ce9f81be9b6f648c516f80bc3688
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 20:33:57 -0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,36 @@
+#maioridade 
+# idade = int(input('Digite a idade :'))
+# print(f' A idade é: {idade}')
+# if idade >= 18:
+#   print(f'Maioridade')
+# else:
+#   print(f' Menoridade')
+
+
+#Usuario informa dois números aleatórios, e o sistema vai comparar se os números sao iguais ou diferentes
+
+# numero1 = int(input('Digite o primeiro número'))
+# numero2 = int(input('Digite o segundo número'))
+
+# print(f'O primiero número é: {numero1}')
+# print(f'O segundo número é: {numero2}')
+
+# if numero1 == numero2:
+#     print(f'Os números são iguais')
+# else:
+#     print(f'Os números são diferentes')
+
+
+#Verificar se um número é positivo, negativo ou zero
+
+# numero = int(input('Digite um número'))
+# print(f'O número digitado é:{numero} ')
+
+# if numero > 0:
+#   print(f'Esse número é positivo')
+
+# elif numero == 0:
+#     print(f'O número é zero')
+# else:
+#     print(f'O número é negativo')
+
